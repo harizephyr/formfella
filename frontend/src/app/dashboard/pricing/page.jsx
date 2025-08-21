dc37f253d81dd35317a49c56bf1314c23352c305
Hariharan S <hariethereal@gmail.com>
2025-08-21 12:53:52 +0530

    Update page.jsx

--- frontend/src/app/dashboard/pricing/page.jsx
+++ frontend/src/app/dashboard/pricing/page.jsx
@@ -35,8 +35,8 @@ const Pricing = () => {
             "amount": 999,
             "currency": "usd",
             "customer_email": "test@gmail.com",
-            "success_url": `/dashboard/pricing/success`,
-            "cancel_url": `/dashboard/pricing/fail`,
+            "success_url": `https://forms.laphic.com/dashboard/pricing/success`,
+            "cancel_url": `https://forms.laphic.com/dashboard/pricing/fail`,
             "mode": "payment"
           }),
         });
